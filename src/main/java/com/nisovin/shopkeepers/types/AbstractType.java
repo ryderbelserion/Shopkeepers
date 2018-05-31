f8e688af82f96b8708a7b5237ac4526eb1ff8c4c
blablubbabc <lukas@wirsindwir.de>
2018-05-31 05:24:02 +0200

    Added Type interface.

--- src/main/java/com/nisovin/shopkeepers/types/AbstractType.java
+++ src/main/java/com/nisovin/shopkeepers/types/AbstractType.java
@@ -5,20 +5,13 @@ import org.bukkit.entity.Player;
 
 import com.nisovin.shopkeepers.util.Utils;
 
-public abstract class AbstractType {
+public abstract class AbstractType implements Type {
 
 	/**
-	 * An unique identifier for this type object.
-	 * <p>
 	 * This could for example be used inside save/configuration files, so it should not contain any characters which
 	 * could cause problems with that.
 	 */
 	protected final String identifier; // not null or empty
-	/**
-	 * The permission a player needs in order to use/access this type in some way.
-	 * <p>
-	 * Can be <code>null</code> to indicate that no permission is needed.
-	 */
 	protected final String permission; // can be null
 
 	protected AbstractType(String identifier, String permission) {
@@ -27,25 +20,27 @@ public abstract class AbstractType {
 		this.permission = Utils.isEmpty(permission) ? null : permission;
 	}
 
+	@Override
 	public final String getIdentifier() {
 		return identifier;
 	}
 
+	@Override
+	public String getPermission() {
+		return permission;
+	}
+
+	@Override
 	public boolean hasPermission(Player player) {
 		return (permission == null || Utils.hasPermission(player, permission));
 	}
 
+	@Override
 	public boolean isEnabled() {
 		return true;
 	}
 
-	/**
-	 * Checks if the given (possibly inaccurate) identifier matches to this type.
-	 * 
-	 * @param identifier
-	 *            an (possible inaccurate) identifier
-	 * @return <code>true</code> if the given identifier is considered to represent this type
-	 */
+	@Override
 	public boolean matches(String identifier) {
 		identifier = Utils.normalize(identifier);
 		return Utils.normalize(this.identifier).equals(identifier);
