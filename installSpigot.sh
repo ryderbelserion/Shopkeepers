794aabb97db03a62a3811350a9f04c4b4cd89b8a
blablubbabc <lukas@wirsindwir.de>
2018-07-20 19:38:52 +0200

    Added some safeguards in installSpigot script, in case it gets accidentally executed.

--- installSpigot.sh
+++ installSpigot.sh
@@ -1,9 +1,23 @@
 #!/usr/bin/env bash
+if [ $# -eq 0 ]; then
+    echo "Missing argument(s)"
+    exit 1
+fi
+
 mkdir -p $HOME/spigot-build
 pushd $HOME/spigot-build
 echo "Downloading Spigot Build Tools for minecraft version $1"
 curl https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar -o $HOME/spigot-build/BuildTools.jar
+
+# Save status of core.autocrlf
+git_autocrlf=`git config --global core.autocrlf`
+# Unset autocrlf, required by BuildTools under linux
 git config --global --unset core.autocrlf
+
+# Build spigot
 echo "Building Spigot using Spigot Build Tools for minecraft version $1 (this might take a while)"
 java -Xmx1500M -jar BuildTools.jar --rev $1 | grep Installing
+
+# Reset autocrlf
+git config --global core.autocrlf $git_autocrlf
 popd
