cb356ab74403023666bd780b8fc3ea7532dfa996
blablubbabc <lukas@wirsindwir.de>
2021-03-01 21:44:34 +0100

    Moved the formatting of Bukkit enum values into a separate helper class.
    Also fixed: We previous checked for the lower case instead of the upper case namespace prefix.

--- src/main/java/com/nisovin/shopkeepers/config/lib/value/types/MinecraftEnumValue.java
+++ src/main/java/com/nisovin/shopkeepers/config/lib/value/types/MinecraftEnumValue.java
@@ -1,18 +1,15 @@
 package com.nisovin.shopkeepers.config.lib.value.types;
 
-import org.bukkit.NamespacedKey;
+import com.nisovin.shopkeepers.util.MinecraftEnumUtils;
 
 /**
- * Extends {@link EnumValue} and removes Minecraft's namespace prefix (i.e. {@link NamespacedKey#MINECRAFT}), if it is
- * present, when parsing the enum value from a given String input.
+ * Extends {@link EnumValue}, but normalizes inputs via {@link MinecraftEnumUtils#normalizeEnumName(String)} when
+ * parsing the enum value from a given input String.
  *
  * @param <E>
  *            the enum type
  */
 public class MinecraftEnumValue<E extends Enum<E>> extends EnumValue<E> {
-
-	// Upper case because we normalize inputs to match the names of enum constants, which are upper case:
-	private static final String MINECRAFT_NAMESPACE_PREFIX = NamespacedKey.MINECRAFT + ":";
 
 	public MinecraftEnumValue(Class<E> enumClass) {
 		super(enumClass);
@@ -20,13 +17,6 @@ public class MinecraftEnumValue<E extends Enum<E>> extends EnumValue<E> {
 
 	@Override
 	protected String normalize(String input) {
-		assert input != null;
-		String normalized = super.normalize(input);
-		// Remove the Minecraft namespace prefix, if it is present:
-		// Note: Any other namespace prefix is not supported in will result in a failed lookup of the enum value.
-		if (normalized.startsWith(MINECRAFT_NAMESPACE_PREFIX)) {
-			normalized = normalized.substring(MINECRAFT_NAMESPACE_PREFIX.length());
-		}
-		return normalized;
+		return MinecraftEnumUtils.normalizeEnumName(input);
 	}
 }
---
+++ src/main/java/com/nisovin/shopkeepers/util/MinecraftEnumUtils.java
@@ -0,0 +1,38 @@
+package com.nisovin.shopkeepers.util;
+
+import java.util.Locale;
+
+import org.bukkit.NamespacedKey;
+
+public class MinecraftEnumUtils {
+
+	// Upper case because we normalize Strings to match the names of enum constants, which are upper case, before we
+	// check for this prefix:
+	private static final String MINECRAFT_NAMESPACE_PREFIX = NamespacedKey.MINECRAFT.toUpperCase(Locale.ROOT) + ":";
+
+	/**
+	 * Formats the given String in a way that matches the names of the Bukkit enum values.
+	 * <p>
+	 * This performs the same transformations as {@link StringUtils#normalizeEnumName(String)}, but additionally strips
+	 * Minecraft's namespace prefix (i.e. {@link NamespacedKey#MINECRAFT}), if it is present.
+	 * <p>
+	 * This returns <code>null</code> if the given input String is <code>null</code>.
+	 * 
+	 * @param enumName
+	 *            the enum name
+	 * @return the formatted enum name
+	 */
+	public static String normalizeEnumName(String enumName) {
+		if (enumName == null) return null;
+		enumName = StringUtils.normalizeEnumName(enumName);
+
+		// Remove the Minecraft namespace prefix, if it is present:
+		if (enumName.startsWith(MINECRAFT_NAMESPACE_PREFIX)) {
+			enumName = enumName.substring(MINECRAFT_NAMESPACE_PREFIX.length());
+		}
+		return enumName;
+	}
+
+	private MinecraftEnumUtils() {
+	}
+}
