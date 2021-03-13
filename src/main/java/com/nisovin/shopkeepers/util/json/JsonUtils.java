38031e8b726845062d31f881daa16bd2a70b23d5
blablubbabc <lukas@wirsindwir.de>
2021-03-13 06:36:45 +0100

    Added utilities to serialize and deserialize objects, including ConfigurationSerializables, in Json format.
    However, there are some shortcomings (missing support for anchors / object references) which make Json unsuited for the reliable (de-)serialization of ConfigurationSerializables in general.

---
+++ src/main/java/com/nisovin/shopkeepers/util/json/JsonUtils.java
@@ -0,0 +1,58 @@
+package com.nisovin.shopkeepers.util.json;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class JsonUtils {
+
+	private static final ThreadLocal<Gson> GSON = ThreadLocal.withInitial(() -> {
+		return newGsonBuilder().create();
+	});
+	private static final ThreadLocal<Gson> GSON_PRETTY = ThreadLocal.withInitial(() -> {
+		return newGsonBuilder().setPrettyPrinting().create();
+	});
+
+	// A GsonBuilder with the common default configuration applied:
+	private static GsonBuilder newGsonBuilder() {
+		return new GsonBuilder()
+				.disableHtmlEscaping()
+				.serializeSpecialFloatingPointValues()
+				.setLenient()
+				.registerTypeAdapterFactory(BukkitAwareObjectTypeAdapter.FACTORY);
+	}
+
+	// The object may be one of the primitives supported by Gson by default (primitive, Map, List, null, ..), or one of
+	// our custom supported types such as ConfigurationSerializable. However, see the limitations mentioned in
+	// BukkitAwareObjectTypeAdapter.
+	public static String toJson(Object object) {
+		return toJson(GSON.get(), object);
+	}
+
+	public static String toPrettyJson(Object object) {
+		return toJson(GSON_PRETTY.get(), object);
+	}
+
+	private static String toJson(Gson gson, Object object) {
+		return gson.toJson(object);
+	}
+
+	// Does not deserialize ConfigurationSerializables.
+	@SuppressWarnings("unchecked")
+	public static <T> T fromPlainJson(String json) throws IllegalArgumentException {
+		Gson gson = GSON.get();
+		try {
+			// This is expected to return Gson's default Object TypeAdapter instead of our custom Bukkit-aware one.
+			return (T) gson.fromJson(json, Object.class);
+		} catch (Exception e) {
+			throw new IllegalArgumentException("Could not deserialize object from Json!", e);
+		}
+	}
+
+	public static <T> T fromJson(String json) throws IllegalArgumentException {
+		Gson gson = GSON.get();
+		return BukkitAwareObjectTypeAdapter.fromJson(gson, json);
+	}
+
+	private JsonUtils() {
+	}
+}
---
+++ src/test/java/com/nisovin/shopkeepers/util/json/JsonSerializationTest.java
@@ -0,0 +1,36 @@
+package com.nisovin.shopkeepers.util.json;
+
+import org.bukkit.inventory.ItemStack;
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.nisovin.shopkeepers.util.AbstractItemStackSerializationTest;
+
+public class JsonSerializationTest extends AbstractItemStackSerializationTest {
+
+	@Override
+	protected String serialize(ItemStack itemStack) {
+		return JsonUtils.toJson(itemStack);
+	}
+
+	@Override
+	protected ItemStack deserialize(Object data) {
+		return JsonUtils.fromJson((String) data);
+	}
+
+	@Test
+	public void testSerializeSpecialFloatingPointNumbers() {
+		testSerializeSpecialFloatingPointNumber(Double.NaN);
+		testSerializeSpecialFloatingPointNumber(Double.POSITIVE_INFINITY);
+		testSerializeSpecialFloatingPointNumber(Double.NEGATIVE_INFINITY);
+		testSerializeSpecialFloatingPointNumber(Double.MAX_VALUE);
+		testSerializeSpecialFloatingPointNumber(Double.MIN_VALUE);
+		// Note: Floats are not preserved during deserialization, because we read all floating point numbers as doubles.
+	}
+
+	private void testSerializeSpecialFloatingPointNumber(Number number) {
+		String json = JsonUtils.toJson(number);
+		Object deserialized = JsonUtils.fromJson(json);
+		Assert.assertEquals(number, deserialized);
+	}
+}
