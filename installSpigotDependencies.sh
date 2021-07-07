b6c736a49046ae631999e4fc61c028696ceddb58
blablubbabc <lukas@wirsindwir.de>
2021-07-07 17:06:21 +0200

    Bumped the CraftBukkit dependencies for the 1_16_R2 and 1_16_R3 versions of Shopkeepers from 1.16.2-R0.1 and 1.16.4-R0.1 to 1.16.3-R0.1 and 1.16.5-R0.1 respectively.
    This has no effect on the server versions the plugin works on, but simplifies testing.

--- installSpigotDependencies.sh
+++ installSpigotDependencies.sh
@@ -9,8 +9,8 @@ source ./installJDK.sh 8
 if [ ! -f "$HOME/.m2/repository/org/bukkit/craftbukkit/1.14.4-R0.1-SNAPSHOT/craftbukkit-1.14.4-R0.1-SNAPSHOT.jar" ]; then ./installSpigot.sh 1.14.4 ; else echo "Not compiling Spigot 1.14.4 because it is already in our Maven repo" ; fi
 if [ ! -f "$HOME/.m2/repository/org/bukkit/craftbukkit/1.15.2-R0.1-SNAPSHOT/craftbukkit-1.15.2-R0.1-SNAPSHOT.jar" ]; then ./installSpigot.sh 1.15.2 ; else echo "Not compiling Spigot 1.15.2 because it is already in our Maven repo" ; fi
 if [ ! -f "$HOME/.m2/repository/org/bukkit/craftbukkit/1.16.1-R0.1-SNAPSHOT/craftbukkit-1.16.1-R0.1-SNAPSHOT.jar" ]; then ./installSpigot.sh 1.16.1 ; else echo "Not compiling Spigot 1.16.1 because it is already in our Maven repo" ; fi
-if [ ! -f "$HOME/.m2/repository/org/bukkit/craftbukkit/1.16.2-R0.1-SNAPSHOT/craftbukkit-1.16.2-R0.1-SNAPSHOT.jar" ]; then ./installSpigot.sh 1.16.2 ; else echo "Not compiling Spigot 1.16.2 because it is already in our Maven repo" ; fi
-if [ ! -f "$HOME/.m2/repository/org/bukkit/craftbukkit/1.16.4-R0.1-SNAPSHOT/craftbukkit-1.16.4-R0.1-SNAPSHOT.jar" ]; then ./installSpigot.sh 1.16.4 ; else echo "Not compiling Spigot 1.16.4 because it is already in our Maven repo" ; fi
+if [ ! -f "$HOME/.m2/repository/org/bukkit/craftbukkit/1.16.3-R0.1-SNAPSHOT/craftbukkit-1.16.3-R0.1-SNAPSHOT.jar" ]; then ./installSpigot.sh 1.16.3 ; else echo "Not compiling Spigot 1.16.3 because it is already in our Maven repo" ; fi
+if [ ! -f "$HOME/.m2/repository/org/bukkit/craftbukkit/1.16.5-R0.1-SNAPSHOT/craftbukkit-1.16.5-R0.1-SNAPSHOT.jar" ]; then ./installSpigot.sh 1.16.5 ; else echo "Not compiling Spigot 1.16.5 because it is already in our Maven repo" ; fi
 
 # The following versions require JDK 16 to build:
 source ./installJDK.sh 16
