736bf90d6a6c11d0a0e6d3730b908cce6bd823c6
blablubbabc <lukas@wirsindwir.de>
2021-08-10 04:43:34 +0200

    BlockFaceUtilsTests was comparing expected and actual values in the wrong order.

--- modules/main/src/test/java/com/nisovin/shopkeepers/util/bukkit/BlockFaceUtilsTests.java
+++ modules/main/src/test/java/com/nisovin/shopkeepers/util/bukkit/BlockFaceUtilsTests.java
@@ -13,52 +13,52 @@ public class BlockFaceUtilsTests {
 
 	@Test
 	public void testBlockFaceToYaw() {
-		assertEquals(BlockFaceUtils.getYaw(BlockFace.SOUTH), 0.0F, EPSILON);
-		assertEquals(BlockFaceUtils.getYaw(BlockFace.SOUTH_SOUTH_WEST), 22.5F, EPSILON);
-		assertEquals(BlockFaceUtils.getYaw(BlockFace.SOUTH_WEST), 45.0F, EPSILON);
-		assertEquals(BlockFaceUtils.getYaw(BlockFace.WEST_SOUTH_WEST), 67.5F, EPSILON);
-		assertEquals(BlockFaceUtils.getYaw(BlockFace.WEST), 90.0F, EPSILON);
-		assertEquals(BlockFaceUtils.getYaw(BlockFace.NORTH), 180.0F, EPSILON);
-		assertEquals(BlockFaceUtils.getYaw(BlockFace.EAST), 270.0F, EPSILON);
+		assertEquals(0.0F, BlockFaceUtils.getYaw(BlockFace.SOUTH), EPSILON);
+		assertEquals(22.5F, BlockFaceUtils.getYaw(BlockFace.SOUTH_SOUTH_WEST), EPSILON);
+		assertEquals(45.0F, BlockFaceUtils.getYaw(BlockFace.SOUTH_WEST), EPSILON);
+		assertEquals(67.5F, BlockFaceUtils.getYaw(BlockFace.WEST_SOUTH_WEST), EPSILON);
+		assertEquals(90.0F, BlockFaceUtils.getYaw(BlockFace.WEST), EPSILON);
+		assertEquals(180.0F, BlockFaceUtils.getYaw(BlockFace.NORTH), EPSILON);
+		assertEquals(270.0F, BlockFaceUtils.getYaw(BlockFace.EAST), EPSILON);
 	}
 
 	@Test
 	public void testYawToBlockFace() {
-		assertEquals(BlockFaceDirections.CARDINAL.fromYaw(0.0F), BlockFace.SOUTH);
-		assertEquals(BlockFaceDirections.CARDINAL.fromYaw(-0.0F), BlockFace.SOUTH);
-		assertEquals(BlockFaceDirections.CARDINAL.fromYaw(40.0F), BlockFace.SOUTH);
-		assertEquals(BlockFaceDirections.CARDINAL.fromYaw(-40.0F), BlockFace.SOUTH);
-		assertEquals(BlockFaceDirections.CARDINAL.fromYaw(-400.0F), BlockFace.SOUTH);
-		assertEquals(BlockFaceDirections.CARDINAL.fromYaw(760.0F), BlockFace.SOUTH);
-		assertEquals(BlockFaceDirections.CARDINAL.fromYaw(90.0F), BlockFace.WEST);
-		assertEquals(BlockFaceDirections.CARDINAL.fromYaw(180.0F), BlockFace.NORTH);
-		assertEquals(BlockFaceDirections.CARDINAL.fromYaw(-180.0F), BlockFace.NORTH);
-		assertEquals(BlockFaceDirections.CARDINAL.fromYaw(270.0F), BlockFace.EAST);
+		assertEquals(BlockFace.SOUTH, BlockFaceDirections.CARDINAL.fromYaw(0.0F));
+		assertEquals(BlockFace.SOUTH, BlockFaceDirections.CARDINAL.fromYaw(-0.0F));
+		assertEquals(BlockFace.SOUTH, BlockFaceDirections.CARDINAL.fromYaw(40.0F));
+		assertEquals(BlockFace.SOUTH, BlockFaceDirections.CARDINAL.fromYaw(-40.0F));
+		assertEquals(BlockFace.SOUTH, BlockFaceDirections.CARDINAL.fromYaw(-400.0F));
+		assertEquals(BlockFace.SOUTH, BlockFaceDirections.CARDINAL.fromYaw(760.0F));
+		assertEquals(BlockFace.WEST, BlockFaceDirections.CARDINAL.fromYaw(90.0F));
+		assertEquals(BlockFace.NORTH, BlockFaceDirections.CARDINAL.fromYaw(180.0F));
+		assertEquals(BlockFace.NORTH, BlockFaceDirections.CARDINAL.fromYaw(-180.0F));
+		assertEquals(BlockFace.EAST, BlockFaceDirections.CARDINAL.fromYaw(270.0F));
 
-		assertEquals(BlockFaceDirections.INTERCARDINAL.fromYaw(0.0F), BlockFace.SOUTH);
-		assertEquals(BlockFaceDirections.INTERCARDINAL.fromYaw(-0.0F), BlockFace.SOUTH);
-		assertEquals(BlockFaceDirections.INTERCARDINAL.fromYaw(20.0F), BlockFace.SOUTH);
-		assertEquals(BlockFaceDirections.INTERCARDINAL.fromYaw(-20.0F), BlockFace.SOUTH);
-		assertEquals(BlockFaceDirections.INTERCARDINAL.fromYaw(-380.0F), BlockFace.SOUTH);
-		assertEquals(BlockFaceDirections.INTERCARDINAL.fromYaw(740.0F), BlockFace.SOUTH);
-		assertEquals(BlockFaceDirections.INTERCARDINAL.fromYaw(45.0F), BlockFace.SOUTH_WEST);
-		assertEquals(BlockFaceDirections.INTERCARDINAL.fromYaw(90.0F), BlockFace.WEST);
-		assertEquals(BlockFaceDirections.INTERCARDINAL.fromYaw(180.0F), BlockFace.NORTH);
-		assertEquals(BlockFaceDirections.INTERCARDINAL.fromYaw(-180.0F), BlockFace.NORTH);
-		assertEquals(BlockFaceDirections.INTERCARDINAL.fromYaw(270.0F), BlockFace.EAST);
+		assertEquals(BlockFace.SOUTH, BlockFaceDirections.INTERCARDINAL.fromYaw(0.0F));
+		assertEquals(BlockFace.SOUTH, BlockFaceDirections.INTERCARDINAL.fromYaw(-0.0F));
+		assertEquals(BlockFace.SOUTH, BlockFaceDirections.INTERCARDINAL.fromYaw(20.0F));
+		assertEquals(BlockFace.SOUTH, BlockFaceDirections.INTERCARDINAL.fromYaw(-20.0F));
+		assertEquals(BlockFace.SOUTH, BlockFaceDirections.INTERCARDINAL.fromYaw(-380.0F));
+		assertEquals(BlockFace.SOUTH, BlockFaceDirections.INTERCARDINAL.fromYaw(740.0F));
+		assertEquals(BlockFace.SOUTH_WEST, BlockFaceDirections.INTERCARDINAL.fromYaw(45.0F));
+		assertEquals(BlockFace.WEST, BlockFaceDirections.INTERCARDINAL.fromYaw(90.0F));
+		assertEquals(BlockFace.NORTH, BlockFaceDirections.INTERCARDINAL.fromYaw(180.0F));
+		assertEquals(BlockFace.NORTH, BlockFaceDirections.INTERCARDINAL.fromYaw(-180.0F));
+		assertEquals(BlockFace.EAST, BlockFaceDirections.INTERCARDINAL.fromYaw(270.0F));
 
-		assertEquals(BlockFaceDirections.SECONDARY_INTERCARDINAL.fromYaw(0.0F), BlockFace.SOUTH);
-		assertEquals(BlockFaceDirections.SECONDARY_INTERCARDINAL.fromYaw(-0.0F), BlockFace.SOUTH);
-		assertEquals(BlockFaceDirections.SECONDARY_INTERCARDINAL.fromYaw(10.0F), BlockFace.SOUTH);
-		assertEquals(BlockFaceDirections.SECONDARY_INTERCARDINAL.fromYaw(-10.0F), BlockFace.SOUTH);
-		assertEquals(BlockFaceDirections.SECONDARY_INTERCARDINAL.fromYaw(-370.0F), BlockFace.SOUTH);
-		assertEquals(BlockFaceDirections.SECONDARY_INTERCARDINAL.fromYaw(730.0F), BlockFace.SOUTH);
-		assertEquals(BlockFaceDirections.SECONDARY_INTERCARDINAL.fromYaw(22.5F), BlockFace.SOUTH_SOUTH_WEST);
-		assertEquals(BlockFaceDirections.SECONDARY_INTERCARDINAL.fromYaw(45.0F), BlockFace.SOUTH_WEST);
-		assertEquals(BlockFaceDirections.SECONDARY_INTERCARDINAL.fromYaw(67.5F), BlockFace.WEST_SOUTH_WEST);
-		assertEquals(BlockFaceDirections.SECONDARY_INTERCARDINAL.fromYaw(90.0F), BlockFace.WEST);
-		assertEquals(BlockFaceDirections.SECONDARY_INTERCARDINAL.fromYaw(180.0F), BlockFace.NORTH);
-		assertEquals(BlockFaceDirections.SECONDARY_INTERCARDINAL.fromYaw(-180.0F), BlockFace.NORTH);
-		assertEquals(BlockFaceDirections.SECONDARY_INTERCARDINAL.fromYaw(270.0F), BlockFace.EAST);
+		assertEquals(BlockFace.SOUTH, BlockFaceDirections.SECONDARY_INTERCARDINAL.fromYaw(0.0F));
+		assertEquals(BlockFace.SOUTH, BlockFaceDirections.SECONDARY_INTERCARDINAL.fromYaw(-0.0F));
+		assertEquals(BlockFace.SOUTH, BlockFaceDirections.SECONDARY_INTERCARDINAL.fromYaw(10.0F));
+		assertEquals(BlockFace.SOUTH, BlockFaceDirections.SECONDARY_INTERCARDINAL.fromYaw(-10.0F));
+		assertEquals(BlockFace.SOUTH, BlockFaceDirections.SECONDARY_INTERCARDINAL.fromYaw(-370.0F));
+		assertEquals(BlockFace.SOUTH, BlockFaceDirections.SECONDARY_INTERCARDINAL.fromYaw(730.0F));
+		assertEquals(BlockFace.SOUTH_SOUTH_WEST, BlockFaceDirections.SECONDARY_INTERCARDINAL.fromYaw(22.5F));
+		assertEquals(BlockFace.SOUTH_WEST, BlockFaceDirections.SECONDARY_INTERCARDINAL.fromYaw(45.0F));
+		assertEquals(BlockFace.WEST_SOUTH_WEST, BlockFaceDirections.SECONDARY_INTERCARDINAL.fromYaw(67.5F));
+		assertEquals(BlockFace.WEST, BlockFaceDirections.SECONDARY_INTERCARDINAL.fromYaw(90.0F));
+		assertEquals(BlockFace.NORTH, BlockFaceDirections.SECONDARY_INTERCARDINAL.fromYaw(180.0F));
+		assertEquals(BlockFace.NORTH, BlockFaceDirections.SECONDARY_INTERCARDINAL.fromYaw(-180.0F));
+		assertEquals(BlockFace.EAST, BlockFaceDirections.SECONDARY_INTERCARDINAL.fromYaw(270.0F));
 	}
 }
