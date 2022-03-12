d0f4ab21122bec1d87b52ae663159229a8a779c4
blablubbabc <lukas@wirsindwir.de>
2022-03-12 16:53:48 +0100

    TextUtils no longer uses System.out directly for logging.

--- modules/main/src/test/java/com/nisovin/shopkeepers/util/TextTest.java
+++ modules/main/src/test/java/com/nisovin/shopkeepers/util/TextTest.java
@@ -1,7 +1,6 @@
 package com.nisovin.shopkeepers.util;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -9,6 +8,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.Set;
+import java.util.logging.Logger;
 
 import org.bukkit.configuration.Configuration;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -21,6 +21,8 @@ import com.nisovin.shopkeepers.util.bukkit.TextUtils;
 import com.nisovin.shopkeepers.util.java.ClassUtils;
 
 public class TextTest extends AbstractBukkitTest {
+
+	private static final Logger LOGGER = Logger.getLogger(TextTest.class.getCanonicalName());
 
 	// Tests the Text parsing and backwards conversion with the actual messages of the default language file
 	@Test
@@ -45,7 +47,7 @@ public class TextTest extends AbstractBukkitTest {
 			assertEquals("Plain format text does not match parsing input for: " + key, stringText, text.toPlainFormatText());
 			assertEquals("Plain text does not match parsing input for: " + key, stringText, text.toPlainText());
 		}
-		System.out.println("Tested config messages: " + tested + " / " + configKeys.size() + " (total config entries)");
+		LOGGER.info("Tested config messages: " + tested + " / " + configKeys.size() + " (total config entries)");
 		assertTrue("The test didn't actually test anything!", tested > 0);
 	}
 }
