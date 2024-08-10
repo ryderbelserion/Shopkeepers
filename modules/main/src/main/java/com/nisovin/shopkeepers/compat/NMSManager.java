d403754cad00486a0abaa4ced4aa659176602288
blablubbabc <lukas@wirsindwir.de>
2024-08-10 12:42:52 +0200

    Also remove no longer supported versions from NMSManager

--- modules/main/src/main/java/com/nisovin/shopkeepers/compat/NMSManager.java
+++ modules/main/src/main/java/com/nisovin/shopkeepers/compat/NMSManager.java
@@ -20,32 +20,20 @@ public final class NMSManager {
 		SUPPORTED_MAPPINGS_VERSIONS.put(version.getMappingsVersion(), version);
 	}
 
-	// Note: Although they look similar, our compat versions do not necessarily match CraftBukkit's
-	// 'Minecraft Version'.
-	// Our revision number (behind the 'R') is incremented with every mappings version change,
-	// whereas CraftBukkit may increment it less frequently.
-	// TODO Since 1.17, we have to update and rebuild our compat code whenever the mappings are
-	// changed. However, Spigot does not necessarily align mappings changes with CraftBukkit version
-	// bumps. The BuildTools also does not allow to build specific commits. We therefore cannot
-	// build our compat modules against specific mappings versions of CraftBukkit yet and can
-	// consequently only support the latest mappings version for every CraftBukkit version.
-	// Versions before 1.17 are not affected by this, because they don't depend on the NMS code
-	// remapping.
+	// We have to update and rebuild our compat code whenever the mappings changed.
+	// Changes to the mappings version do not necessarily align with a bump of the CraftBukkit
+	// version. To reduce the number of CraftBukkit versions to depend on and build, we only support
+	// the latest mappings version for every CraftBukkit version.
+	// Although they look similar, our compat versions do not necessarily match CraftBukkit's
+	// 'Minecraft Version': Our revision number (behind the 'R') is incremented for every new compat
+	// module for a specific major Minecraft version, which usually aligns with mappings updates for
+	// new minor Minecraft updates, whereas CraftBukkit may increment its 'Minecraft Version' less
+	// frequently.
 	static {
 		// Registered in the order from latest to oldest.
 		register(new CompatVersion("1_21_R1", "1.21", "229d7afc75b70a6c388337687ac4da1f"));
 		// Note: MC 1.20.6 completely replaced 1.20.5. We only support 1.20.6.
 		register(new CompatVersion("1_20_R5", "1.20.6", "ee13f98a43b9c5abffdcc0bb24154460"));
-		register(new CompatVersion("1_20_R4", "1.20.4", "60a2bb6bf2684dc61c56b90d7c41bddc"));
-		register(new CompatVersion("1_20_R3", "1.20.2", "3478a65bfd04b15b431fe107b3617dfc"));
-		register(new CompatVersion("1_20_R2", "1.20.1", "bcf3dcb22ad42792794079f9443df2c0"));
-		register(new CompatVersion("1_19_R5", "1.19.4", "3009edc0fff87fa34680686663bd59df"));
-		// Different CB revision:
-		register(new CompatVersion("1_18_R3", "1.18.2", "eaeedbff51b16ead3170906872fda334"));
-		// Different CB revision:
-		register(new CompatVersion("1_17_R2", "1.17.1", "f0e3dfc7390de285a4693518dd5bd126"));
-		register(new CompatVersion("1_16_R3", "1.16.5", "d4b392244df170796f8779ef0fc1f2e9"));
-		register(new CompatVersion("1_16_R3", "1.16.5", "54e89c47309b53737f894f1bf1b0edbe"));
 	}
 
 	public static @Nullable CompatVersion getCompatVersion(String compatVersion) {
